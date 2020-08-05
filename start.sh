58e966ecc987f540fab02254214e0428fcf231a9
Michael Yeates <michaeljyeates@gmail.com>
2020-08-05 12:31:27 +0100

    updates

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ CLEOS=/home/mike/Projects/EOS/jungle.sh
 
 TIME=$1
 COUNT=500
-ITR=200
-DATA_SIZE=16384
+ITR=100
+DATA_SIZE=500000
 
 i=0
 while [ $i -lt $ITR ]
@@ -15,4 +15,5 @@ RND2=$((1 + RANDOM % 1000000000000))
 echo "[$COUNT, $RND$RND2, $TIME, $DATA_SIZE]" > load.json
 $CLEOS push action ohnoesnothim l load.json -p ohnoesnothim
 i=$[$i+1]
+sleep 0.1
 done
